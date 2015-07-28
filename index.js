df91871c011f2748729518fe3b899504723aa63b
Victor Kulebyakin <victor@pingbull.com>
2015-07-28 09:53:19 +0300

    fix insertAfter

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ var LinkedList = require('./lib/LinkedList'),
     lM = new LinkedListMap(),
     lWM = new LinkedListWeakMap();
 
-lWM.unshift({
-    test: 1
-}).unshift([2, 3, 4])
+lWM.unshift(1)
+    .unshift({
+        test: 1
+    }).unshift([2, 3, 4])
     .unshift('5')
-    .unshift(6);
+    .unshift(6)
+    .unshift(7)
+    .unshift(7);
 
 console.log(lWM.shift(), lWM);
 
-lWM.forEach(function (node) {
-    console.log(node);
+lWM.forEach(function (v, k) {
+    if (v === 1) {
+        lWM.insertAfter(k, 25);
+    }
+});
+
+lWM.forEach(function (v, k) {
+    console.log(v, k);
 });
