3e2f568bba47022c59b4a612adafcb66d1e37025
Victor Kulebyakin <victor@pingbull.com>
2015-08-04 21:36:15 +0300

    change to LinkedList based on WeakMap

---
+++ test/index.js
@@ -0,0 +1,48 @@
+var should = require('chai').should(),
+    LinkedList = require('../'),
+    list;
+
+describe('LinkedList', function() {
+    describe('#unshift', function () {
+
+    });
+    describe('#shift', function () {
+
+    });
+    describe('#remove', function () {
+
+    });
+    describe('#prepend', function () {
+
+    });
+    describe('#forEach', function () {
+
+    });
+    describe('#reverse', function () {
+
+    });
+    describe('#insertBefore', function () {
+
+    });
+    describe('#insertAfter', function () {
+
+    });
+    describe('#join', function () {
+
+    });
+    describe('#toString', function () {
+
+    });
+    describe('#valueOf', function () {
+
+    });
+    describe('#isEmpty', function () {
+
+    });
+    describe('#Symbol.iterator', function () {
+
+    });
+    describe('#isLinkedList', function () {
+
+    });
+});
